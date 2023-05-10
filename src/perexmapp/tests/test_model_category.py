12829488dc32e86226f78b6398b28d5943005d8a
Liliana Loaiza <liliana.loaiza@globant.com>
2023-05-10 07:36:02 -0500

    :test_tube: Add fixture for category tests

--- src/perexmapp/tests/test_model_category.py
+++ src/perexmapp/tests/test_model_category.py
@@ -8,14 +8,21 @@ from perexmapp.models import Category, BaseModel
 # pylint: disable=no-member
 
 
+@pytest.fixture
+def category():
+    """Fixture for category creation"""
+
+    my_category = Category.objects.create(name='Test Category')
+    yield my_category
+    my_category.delete()
+
+
 @pytest.mark.django_db
-def test_category_success():
+def test_category_success(category):
     """Test a category is successfully created"""
 
-    category_name = 'Category Lalo'
-    category = Category.objects.create(name=category_name)
     assert category.id is not None
-    assert category.name == category_name
+    assert 0 < len(category.name) < 61
     assert category.deleted is False
     assert category.deleted_at is None
 
@@ -24,56 +31,49 @@ def test_category_success():
 def test_category_without_name():
     """Tests when category has no name"""
 
-    with pytest.raises(IntegrityError) as excinfo:
+    with pytest.raises(IntegrityError):
         Category.objects.create(name=None)
-        assert 'name' in excinfo.value.message_dict
 
 
 @pytest.mark.django_db
 def test_category_name_exceeds_length():
     """Tests when category name exceeds its length"""
 
-    category = Category.objects.create(name="a" * 60)
-    assert len(category.name) <= 60
+    obj_category = Category.objects.create(name="a" * 60)
+    assert len(obj_category.name) <= 60
 
     with pytest.raises(DataError):
         Category.objects.create(name="a" * 61)
 
 
 @pytest.mark.django_db
-def test_category_soft_delete():
+def test_category_soft_delete(category):
     """Test soft-delete is performed when delete action is called"""
 
-    category = Category.objects.create(name='Test Category')
     category.delete()
-    category.refresh_from_db()
-
     assert category.deleted is True
     assert category.deleted_at is not None
 
 
 @pytest.mark.django_db
-def test_category_undo_soft_delete():
+def test_category_undo_soft_delete(category):
     """Undoing soft-deletion of category entity"""
 
-    category = Category.objects.create(id=1)
     category.undelete()
-    category.refresh_from_db()
-
     assert category.deleted is False
     assert category.deleted_at is None
 
 
 def test_category__str__returns_name():
-    """Test __str__ method returns the category name"""
+    """Test __str__ method returns the category name."""
 
-    category = Category()
-    category.name = 'CategoryTest'
-    assert str(category) == category.name
+    obj_category = Category()
+    obj_category.name = 'CategoryTest'
+    assert str(obj_category) == obj_category.name
 
 
 def test_category_verbose_name_plural():
-    """Tests if verbose name plural form is 'Categories'"""
+    """Tests if verbose name plural form is 'Categories'."""
 
     verbose_name_plural = str(Category.objects.model._meta.verbose_name_plural)
     assert verbose_name_plural == 'Categories'
